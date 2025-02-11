cd27c96e0e0322e0444bbb6af7c9f92703b31a80
mukul-truwix <mukuls@truwix.com>
2025-02-11 13:23:57 +0530

    changes

--- server.js
+++ server.js
@@ -1,48 +1,46 @@
+const mongoose = require("mongoose");
+const path = require("path");
+const dotenv = require("dotenv");
+const moment = require("moment-timezone");
+const os = require("os");
+const { NODE_ENV = "LOCAL" } = process.env;
+const configModel = require("./models/configModel");
+const { logger } = require("./utils/logger");
 
-const mongoose = require('mongoose');
-const path = require('path');
-const dotenv = require('dotenv');
-const moment =  require('moment-timezone');
-const os = require('os');
-const { NODE_ENV = 'LOCAL' } = process.env;
-const configModel = require('./models/configModel');
-const {logger} = require('./utils/logger');
-
-require('dotenv').config();
+require("dotenv").config();
 const mongo_url = process.env.MONGO_CONN;
 
 const connectToMongoDB = async () => {
-    try {
-        await mongoose.connect(mongo_url, {
-            // useNewUrlParser: true,
-            // useUnifiedTopology: true,
-        });
-        // logger.debug('Connected to Iskcon MongoDB');
-        // global['config'] = configModel.findOne({}).select('-_id').lean().exec();
-        // HeartBeat Check
-        // if (['UAT', 'PRODUCTION'].includes(NODE_ENV.toUpperCase())) {
-        //     const configStream = configModel.watch();
+  try {
+    await mongoose.connect(mongo_url, {
+      // useNewUrlParser: true,
+      // useUnifiedTopology: true,
+    });
+    // logger.info('Connected to Iskcon MongoDB');
+    // global['config'] = configModel.findOne({}).select('-_id').lean().exec();
+    // HeartBeat Check
+    // if (['UAT', 'PRODUCTION'].includes(NODE_ENV.toUpperCase())) {
+    //     const configStream = configModel.watch();
 
-        //     configStream.on('change', async () => {
-        //         try {
-        //             // Fetch the configuration from the database
-        //             global['config'] = await configModel.findOne({}).select('-_id').lean().exec();
-            
-        //             // Log success message with the host's name
-        //            logger.debug(`Config Set Successfully on Host ${os.hostname()}`);
-        //         } catch (error) {
-        //             logger.error('Error setting config:', error);
-        //         }
-        //     });
-            
-        // }
-        logger.debug('Hello')
-        require('./app');
-    } catch (err) {
-        logger.error('Error connecting to Iskcon MongoDB:', err);
-    }
+    //     configStream.on('change', async () => {
+    //         try {
+    //             // Fetch the configuration from the database
+    //             global['config'] = await configModel.findOne({}).select('-_id').lean().exec();
+
+    //             // Log success message with the host's name
+    //            logger.info(`Config Set Successfully on Host ${os.hostname()}`);
+    //         } catch (error) {
+    //             logger.error('Error setting config:', error);
+    //         }
+    //     });
+
+    // }
+    require("./app");
+  } catch (err) {
+    logger.error("Error connecting to Iskcon MongoDB:", err);
+  }
 };
 
 connectToMongoDB();
 
-mongoose.set('debug', process.env.NODE_ENV !== 'production');
+mongoose.set("debug", process.env.NODE_ENV !== "production");
--- utils/logger.js
+++ utils/logger.js
@@ -1,15 +1,15 @@
-const fs = require('fs');
-const pino = require('pino');
-const moment = require('moment-timezone');
-const pinoHttp = require('pino-http');
-const requestContext = require('request-context');
-require('dotenv').config();
+const fs = require("fs");
+const pino = require("pino");
+const moment = require("moment-timezone");
+const pinoHttp = require("pino-http");
+const requestContext = require("request-context");
+require("dotenv").config();
 
 // Retrieve log level from environment variables or default to 'info'
-const defaultLogLevel = process.env.LOG_LEVEL || 'info';
+const defaultLogLevel = process.env.LOG_LEVEL || "info";
 
 const logLevels = {
-    levels: {
+  levels: {
     trace: 10,
     debug: 20,
     info: 30,
@@ -20,27 +20,71 @@ const logLevels = {
   useOnlyCustom: true,
 };
 
-
 if (!logLevels.levels[defaultLogLevel]) {
-  throw new Error(`Default level '${defaultLogLevel}' must be included in custom levels.`);
+  throw new Error(
+    `Default level '${defaultLogLevel}' must be included in custom levels.`
+  );
 }
 
+// Define log destination
 const logDestination = process.stdout; // Change to a file stream if needed
-const logger = pino({
-  customLevels: logLevels.levels,
-  useOnlyCustomLevels: logLevels.useOnlyCustom,
-  level: defaultLogLevel,
-  mixin() {
-    return {
-      requestId: requestContext.get('apirequest:requestid') || '',
-      client: requestContext.get('apirequest:client') || '',
-      user: requestContext.get('apirequest:iskconUser') || '',
-    };
-  },
-  redact: ['req.headers["x-api-key"]', 'req.headers["dm_token"]'],
-  timestamp: () => `,\"timestamp\":\"${moment().tz("Asia/Kolkata").format('YYYY-MM-DDTHH:mm:ss.SSS')}\"`,
-}, pino.destination(logDestination));
 
+// Custom Logger
+const logger = pino(
+  {
+    customLevels: logLevels.levels,
+    useOnlyCustomLevels: logLevels.useOnlyCustom,
+    level: defaultLogLevel,
+    mixin() {
+      return {
+        requestId: requestContext.get("apirequest:requestid") || "",
+        client: requestContext.get("apirequest:client") || "",
+        user: requestContext.get("apirequest:iskconUser") || "",
+      };
+    },
+    redact: ["req.headers['x-api-key']", "req.headers['dm_token']"],
+    timestamp: () =>
+      `,"timestamp":"${moment()
+        .tz("Asia/Kolkata")
+        .format("YYYY-MM-DDTHH:mm:ss.SSS")}"`,
+  },
+  logDestination
+);
+
+const originalError = logger.error;
+logger.error = function (...args) {
+  const [message, error] = args;
+  if (error instanceof Error) {
+    return originalError.call(logger, {
+      message,
+      errorMessage: error.message,
+      stack: error.stack,
+    });
+  }
+  return originalError.call(logger, message);
+};
+
+const originalWarn = logger.warn;
+logger.warn = function (...args) {
+  const [message, data] = args;
+  if (data) {
+    return originalWarn.call(logger, { message, data });
+  }
+  return originalWarn.call(logger, message);
+};
+
+const originalDebug = logger.debug;
+logger.debug = function (...args) {
+  if (args.length > 1) {
+    return originalDebug.call(logger, {
+      message: args[0],
+      data: args.slice(1),
+    });
+  }
+  return originalDebug.call(logger, ...args);
+};
+
+// Express Logger
 const expressLogger = pinoHttp({
   logger: logger,
   serializers: {
@@ -55,5 +99,3 @@ module.exports = {
   expressLogger,
   logger,
 };
-
- 
